57e4e745f0fe3f90ea0b057cd5760723ff7e0dd5
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2025-10-28 08:25:36 +0700

    Setup spring boot project

---
+++ src/main/java/org/example/java6n_fa25/Java6NFa25Application.java
@@ -0,0 +1,14 @@
+package org.example.java6n_fa25;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java6NFa25Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Java6NFa25Application.class, args);
+        System.out.println("running...");
+    }
+
+}
---
+++ src/test/java/org/example/java6n_fa25/Java6NFa25ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.java6n_fa25;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java6NFa25ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
